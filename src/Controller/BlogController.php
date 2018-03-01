9ef846be01f7067240ac227c9da3deb2a2ce38a5
Mathieu Santostefano <msantostefano@jolicode.com>
2018-03-01 12:23:07 +0100

    Add Twig views

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -12,21 +12,38 @@ class BlogController extends Controller
     /**
      * @Route("/", name="home")
      */
-    public function index(Request $request)
+    public function index()
     {
-        return new Response('<html><body>
-            <h1 style="text-align: center; font-family: sans-serif; margin-top: 150px;">Welcome to your homepage '. $request->query->get('name') .'!</h1>
-            <img src="https://heavyeditorial.files.wordpress.com/2018/02/screen-shot-2018-02-06-at-4-56-27-pm1.jpg" alt="starman" style="margin: 100px auto; display: block;">
-        </body></html>');
+        return new Response('
+<html>
+    <body>
+        <h1 style="text-align: center; font-family: sans-serif; margin-top: 150px;">
+            Welcome to your blog controller! 
+        </h1>
+    </body>
+</html>');
     }
 
     /**
      * @Route("/blog", name="blog")
      */
-    public function blog()
+    public function blog(Request $request)
     {
-        return new Response('<html><body><h1 style="text-align: center; font-family: sans-serif; margin-top: 150px;">Welcome to your blog controller!</h1></body></html>');
+        $name = $request->query->get('name');
+
+        return $this->render('blog.html.twig', [
+            'blog_title' => 'Mon super blog',
+            'name' => $name,
+        ]);
     }
 }
+
+
+
+
+
+
+
+
 
 
